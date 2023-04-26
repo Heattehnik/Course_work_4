11f8c83ac92f66d36cf7b3eec9bfb9989f5b97ac
Heattehnik <gadjka@mail.ru>
2023-04-26 21:47:44 +0500

    get_vacancies()

--- src/api.py
+++ src/api.py
@@ -28,9 +28,12 @@ class SuperJobAPI(APIConnector):
     def __init__(self):
         self.response = None
         self.token = os.getenv('SJ_TOKEN')
-
+        self.client_id = os.getenv('SJ_CLIENT_ID')
+        self.headers = {'X-Api-App-Id': self.token}
     def get_vacancies(self) -> dict:
-        self.response = req.get('https://api.superjob.ru/2.0/vacancies/?keyword=%D0%91%D1%83%D1%85%D0%B3%D0%B0%D0%BB%D1%82%D0%B5%D1%80&order_field=payment&order_direction=asc&payment_from=10000&payment_to=300000&no_agreement=1&town=4&catalogues=33%2C151%2C11%2C438%2C327%2C306%2C478%2C86&place_of_work=1&moveable=1&agency=1&type_of_work=6&age=30&gender=2&education=2&experience=3&driving_licence%5B0%5D=B&driving_licence%5B1%5D=C&language=1&lang_level=3&languages_particular=1')
+        keywords = ['python developer']
+        self.response = req.get(f'https://api.superjob.ru/2.0/vacancies/?keywords={keywords}&count=100&page=1&published=3&archive=1',
+                                headers=self.headers)
         return self.response.json()
 
 
