d13af3abb37bde6f78e7b92e4290b6e2fdefc795
Heattehnik <gadjka@mail.ru>
2023-05-11 14:39:46 +0500

    work with db

--- src/vacancies.py
+++ src/vacancies.py
@@ -1,5 +1,56 @@
+from abc import ABC, abstractmethod
+import sqlite3
+
+connect = sqlite3.connect('../data/main.db', check_same_thread=False)
+cursor = connect.cursor()
 
 
-class Vacancy:
-    def __init__(self, vacancy: dict) -> None:
-        self.title = vacancy['title']
+class Vacancy(ABC):
+    all_vacancies = []
+
+    def __init__(self, platform, vacancy_id, title, description, salary_from, salary_to, employer, url) -> None:
+        self.platform = platform
+        self.vacancy_id = vacancy_id
+        self.title = title
+        self.description = description
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.employer = employer
+        self.url = url
+        self.all_vacancies.append(self)
+
+    @abstractmethod
+    def insert_vacancy(self):
+        pass
+
+    @abstractmethod
+    def search_vacancy(self):
+        pass
+
+    @abstractmethod
+    def delete_from_db(self):
+        pass
+
+
+class HHVacancy(Vacancy):
+
+    def insert_vacancy(self):
+        pass
+
+    def search_vacancy(self):
+        pass
+
+    def delete_from_db(self):
+        pass
+
+
+class SJVacancy(Vacancy):
+
+    def insert_vacancy(self):
+        pass
+
+    def search_vacancy(self):
+        pass
+
+    def delete_from_db(self):
+        pass
